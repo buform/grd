3a4f20e8320a8154ee9ab6edc57bb51211ea6652
buform <155317043+buform@users.noreply.github.com>
2025-12-26 14:56:46 +0100

    Refactor random number generator and file saving

--- main_tree/version_3-1/randomiser_3-1.cpp
+++ main_tree/version_3-1/randomiser_3-1.cpp
@@ -4,53 +4,79 @@
 #include <cmath>
 #include <string>
 #include <ctime>
+#include <climits>
 
 void printHeader() {
-  std::cout
-    << "=====================================================\n"
-    << "= SIMPLE ALGORITHM TO GENERATE RANDOM DIGITS IN C++ =\n"
-    << "=====================================================\n";
+	std::cout
+		<< "=====================================================\n"
+		<< "= SIMPLE ALGORITHM TO GENERATE RANDOM DIGITS IN C++ =\n"
+		<< "=====================================================\n";
 }
 
-int getInt(const std::string& promt, in min = INT_MIN, int max = INT_MAX) {
-  int value;
+int getInt(const std::string& prompt, int min = INT_MIN, int max = INT_MAX) {
+	int value;
 
-  while (true) {
-    std::cout << prompt;
-    if (std::cin >> value) {
-      if (value >= min && value <= max)
-        return value;
-      else
-        std::cout << "Value out of range [" << min << "-" << max << "]!\n";
-    }
-    else {
-      std::cout << "Invalid input!\n";
-      std::cin.clear();
-      std::cin.ignore(10000, '\n');
-    }
-  }
-}
-
-void generateRandomNumbers() {
-  const int max_amount 1000000;
-  const int max_lenght = 10;
-
-  int amount_digits = getInt("Enter amount of random digits: ", 1, max_amount);
-  int lenght_digits = getInt("Enter max lenght of number: ", 1, max_lenght);
-
-  int *numbers = new int[amount_digits];
-
-  int power = (int)pow(10, lenght_digits);
+	while (true) {
+		std::cout << prompt;
+		if (std::cin >> value) {
+			if (value >= min && value <= max)
+			return value;
+		else
+			std::cout << "Value out of range [" << min << "-" << max << "]!\n";
+		}
+		else {
+			std::cout << "Invalid input!\n";
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+		}
+	}
 }
 
 void saveToFile(int* numbers, int size) {
+	std::string file_name = "return.txt";
+	std::ofstream file(file_name);
 
+	if (file.is_open()) {
+		for (int i = 0; i < size; i++)
+			file << numbers[i] << "\n";
+		file.seekp(0, std::ios::end);
+		long long size = file.tellp();
+		std::cout << "The file is saved as " << file_name << ".\n";
+		std::cout << size << " byte(s)\n";
+	}
+
+	else {
+		std::cout << "ERROR - cannot open the file!\n";
+	}
+
+	file.close();
+}
+
+void generateRandomNumbers() {
+	const int max_amount = 1000000;
+	const int max_lenght = 10;
+
+	int amount_digits = getInt("Enter amount of random digits: ", 1, max_amount);
+	int lenght_digits = getInt("Enter max lenght of number: ", 1, max_lenght);
+
+	int *numbers = new int[amount_digits];
+	int power = (int)pow(10, lenght_digits);
+
+	std::cout << "Here your digits, they are saved in file.\n";
+
+	for (int i = 0; i < amount_digits; i++) {
+		numbers[i] = rand() % power;
+		std::cout << numbers[i] << "\n";
+	}
+
+	saveToFile(numbers, amount_digits);
+	delete[] numbers;
 }
 
 int main() {
-  srand((unsigned int)time(nullptr));
-  printHeader();
-  generateRandomNumbers()
+	srand((unsigned int)time(nullptr));
+	printHeader();
+	generateRandomNumbers();
 
 	return 0;
 }
